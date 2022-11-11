ba83d2780a6752fc03ad7da7ce917c507ee41ec5
Antonio Peixe <antonio.icr.peixe@gmail.com>
2022-11-11 00:13:33 +0000

    chore(nvim): add keymap to clear all breakpoints

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -64,6 +64,7 @@ keymap("n", "<F2>", ":DapStepInto<CR>", opts)
 keymap("n", "<F3>", ":DapStepOut<CR>", opts)
 keymap("n", "<F12>", ":DapTerminate<CR>", opts)
 keymap("n", "<leader>b", ":DapToggleBreakpoint<CR>", opts)
+keymap("n", "<leader>bl", ":lua require'dap'.clear_breakpoints()<CR>", opts)
 keymap("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint Condition: '))<CR>", opts)
 keymap("n", "<leader>dp", ":lua require'dap-python'.test_method()<CR>", opts)
 -- toggle won't close the repl correctly use open and close
