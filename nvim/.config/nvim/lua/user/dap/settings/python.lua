784cb67e44bce3db96ec745b5ffef803c40c06fb
Antonio Peixe <antonio.icr.peixe@gmail.com>
2022-11-11 00:11:48 +0000

    chore(nvim): dap debugpy path to mason venv

--- nvim/.config/nvim/lua/user/dap/settings/python.lua
+++ nvim/.config/nvim/lua/user/dap/settings/python.lua
@@ -3,5 +3,5 @@ if not dappy_status_ok then
     return
 end
 
-dappy.setup()
+dappy.setup(os.getenv("HOME") .. '/.local/share/nvim/mason/packages/debugpy/venv/bin/python')
 dappy.test_runner = 'pytest'
