f0f7418221df4479acb690cce853f6c8d991e244
Antonio Peixe <antonio.icr.peixe@gmail.com>
2022-12-22 07:27:46 +0000

    fix(nvim): don't show thirdparty workspace popup

--- nvim/.config/nvim/lua/antpeixe/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/antpeixe/lsp/settings/sumneko_lua.lua
@@ -6,6 +6,7 @@ return {
             },
             workspace = {
                 -- server aware of Neovim runtime files
+                checkThirdParty = false,
                 library = vim.api.nvim_get_runtime_file("", true)
             },
         },
