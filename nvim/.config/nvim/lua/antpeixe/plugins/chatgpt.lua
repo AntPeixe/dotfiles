6fcaa619fbc54c2101a33b8477e9ecde6eb8866b
Antonio Peixe <antonio.icr.peixe@gmail.com>
2023-08-20 22:48:05 +0100

    feat(nvim): add chapgpt

---
+++ nvim/.config/nvim/lua/antpeixe/plugins/chatgpt.lua
@@ -0,0 +1,11 @@
+return {
+    "jackMort/ChatGPT.nvim",
+    dependencies = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    event = "VeryLazy",
+    -- config = function()
+    --     require("chatgpt").setup()
+    -- end
+    opts = {
+        popup_layout = { default = "right" }
+    }
+}
