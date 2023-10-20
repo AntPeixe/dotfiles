2b6ca7fb1f65dba0695b50761cf313e5fc891ef3
Antonio Peixe <antonio.icr.peixe@gmail.com>
2023-10-20 16:11:48 +0100

    chore(nvim): chatgpt config

--- nvim/.config/nvim/lua/antpeixe/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/antpeixe/plugins/chatgpt.lua
@@ -1,11 +1,16 @@
 return {
     "jackMort/ChatGPT.nvim",
-    dependencies = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
     event = "VeryLazy",
     -- config = function()
     --     require("chatgpt").setup()
     -- end
     opts = {
-        popup_layout = { default = "right" }
+        popup_layout = {
+            default = "right",
+            right = {
+                width = "40%",
+            },
+        }
     }
 }
