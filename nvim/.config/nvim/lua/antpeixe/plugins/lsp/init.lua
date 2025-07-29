16b2051acb2ce50079fcaaca4038c01f1a468ba1
Antonio Peixe <antonio.peixe@du.co>
2025-07-29 22:22:19 +0100

    fix(nvim): mason and lspconfig

--- nvim/.config/nvim/lua/antpeixe/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/antpeixe/plugins/lsp/init.lua
@@ -1,44 +1,45 @@
-local luals_settings = require("antpeixe.plugins.lsp.settings.lua_ls")
-local pyright_settings = require("antpeixe.plugins.lsp.settings.pyright")
-local gopls_settings = require("antpeixe.plugins.lsp.settings.gopls")
-
 return {
-    -- nvim-lspconfig
+    -- mason-lspconfig
     {
-        "neovim/nvim-lspconfig",
+        "mason-org/mason-lspconfig.nvim",
+        version="^1.0.0",
         dependencies = {
             "mason-org/mason.nvim",
-            { "mason-org/mason-lspconfig.nvim", version="^1.0.0" }
+            "neovim/nvim-lspconfig",
         },
-        opts = {
-            servers = {
+        opts = function()
+
+            local lsphandler = require("antpeixe.plugins.lsp.handler")
+            local luals_settings = require("antpeixe.plugins.lsp.settings.lua_ls")
+            local pyright_settings = require("antpeixe.plugins.lsp.settings.pyright")
+            local gopls_settings = require("antpeixe.plugins.lsp.settings.gopls")
+
+            local servers = {
                 lua_ls = luals_settings,
                 pyright = pyright_settings,
                 rust_analyzer = {},
                 gopls = gopls_settings,
-            },
-        },
-        config = function(_, opts)
+            }
+
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-            local on_attach = require("antpeixe.plugins.lsp.handler").on_attach
-
-            local servers = opts.servers
-            require("mason-lspconfig").setup({
+            lsphandler.setup()
+            local ret = {
               ensure_installed = vim.tbl_keys(servers),
               handlers = {
                 function(server_name)
-                  require("lspconfig")[server_name].setup({
+                  vim.lsp.config(server_name, {
                     capabilities = capabilities,
-                    on_attach = on_attach,
+                    on_attach = lsphandler.on_attach,
                     settings = servers[server_name],
                     filetypes = (servers[server_name] or {}).filetypes,
                   })
                 end,
               },
-            })
-            require("antpeixe.plugins.lsp.handler").setup()
+            }
+            return ret
+
         end,
     },
 }
